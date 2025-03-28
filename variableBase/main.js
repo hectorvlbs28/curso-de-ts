a89678a058c8f65fe22b9b3e0fc3c5ae9600cb45
hectorvlbs28 <98681806+hectorvlbs28@users.noreply.github.com>
2025-03-28 12:29:30 -0700

    Tipos primitivos: string, number, boolean, null, undefined de Typescript

---
+++ variableBase/main.js
@@ -0,0 +1,8 @@
+var worldName = 'John Doe';
+var age = 30;
+var isStudent = true;
+var hobbies = ['reading', 'gaming', 'coding'];
+console.log("Hello, ".concat(worldName, "!"));
+console.log("Age, ".concat(age));
+console.log("Is Student, ".concat(isStudent));
+console.log("Hobbies, ".concat(hobbies));
---
+++ variableBase/main.ts
@@ -0,0 +1,9 @@
+let worldName: string = 'John Doe';
+let age: number = 30;
+let isStudent: boolean = true;
+let hobbies: string[] = ['reading', 'gaming', 'coding'];
+
+console.log(`Hello, ${worldName}!`);
+console.log(`Age, ${age}`);
+console.log(`Is Student, ${isStudent}`);
+console.log(`Hobbies, ${hobbies}`);
