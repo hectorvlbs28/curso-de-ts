5336318d58bf552fc471e1e7682185bb4e1f746c
hectorvlbs28 <98681806+hectorvlbs28@users.noreply.github.com>
2025-03-28 12:55:35 -0700

    Declaración de funciones, tipado de parámetros y valores de retorno

---
+++ metodos/metodos.js
@@ -0,0 +1,8 @@
+function printMsg(msg) {
+    console.log(msg);
+}
+printMsg('Hello, TypeScript!');
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 10));
---
+++ metodos/metodos.ts
@@ -0,0 +1,9 @@
+function printMsg(msg: string) {
+  console.log(msg);
+}
+printMsg('Hello, TypeScript!');
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+console.log(sum(5, 10));
